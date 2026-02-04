5c54f07a0637d078c6b06d904eb27244b8d03a4d
Sakshi Babar <sakshibabar17@gmail.com>
2026-02-04 20:47:41 +0530

    Implement Fibonacci series generator in Python
    Fix variable name and add input prompt for series length.

---
+++ Fibonnaci.py
@@ -0,0 +1,9 @@
+#Python program to print fibonnaci series
+
+num = int(input("Enter the number: "))
+a,b = 0,1
+print("Fibonnaci series")
+
+for i in range(n):
+  print(a, end = " ")
+  a, b = b, a + b
