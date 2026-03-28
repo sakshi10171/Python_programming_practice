c523cec63e5b4a8d4910dcc51d37fe27e4408b0c
Sakshi Babar <sakshibabar17@gmail.com>
2026-03-28 22:35:10 +0530

    Create encapsulation.py

---
+++ oops/encapsulation.py
@@ -0,0 +1,16 @@
+# Encapsulation example
+
+class Student:
+    def __init__(self):
+        self.__marks = 0   # private variable
+
+    def set_marks(self, marks):
+        self.__marks = marks
+
+    def get_marks(self):
+        return self.__marks
+
+s = Student()
+s.set_marks(85)
+
+print("Marks:", s.get_marks())
