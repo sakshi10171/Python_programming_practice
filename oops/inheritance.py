95a53612c2bb79b0757d10dad18cc87026566582
Sakshi Babar <sakshibabar17@gmail.com>
2026-03-28 22:35:45 +0530

    Create inheritance.py

---
+++ oops/inheritance.py
@@ -0,0 +1,13 @@
+# Inheritance example
+
+class Animal:
+    def eat(self):
+        print("Eating...")
+
+class Dog(Animal):
+    def bark(self):
+        print("Barking...")
+
+d = Dog()
+d.eat()
+d.bark()
