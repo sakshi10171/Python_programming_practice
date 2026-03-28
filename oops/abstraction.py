43971d565c8016562e7c49d41759b52bfb81a048
Sakshi Babar <sakshibabar17@gmail.com>
2026-03-28 22:37:08 +0530

    Create abstraction.py

---
+++ oops/abstraction.py
@@ -0,0 +1,16 @@
+# Abstraction example
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    def sound(self):
+        print("Barking...")
+
+d = Dog()
+d.sound()
